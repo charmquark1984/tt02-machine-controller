0283bab55b2d5b7e7dfa810542964e36854cfaee
charmquark1984 <charmquark1984@users.noreply.github.com>
2022-11-13 18:05:21 -0700

    change tb to actually use my code...

--- src/tb.v
+++ src/tb.v
@@ -26,7 +26,7 @@ module tb (
     assign segments = outputs[6:0];
 
     // instantiate the DUT
-    seven_segment_seconds #(.MAX_COUNT(100)) seven_segment_seconds(
+    charmquark1984_controller #(.MAX_COUNT(100)) charmquark1984_controller(
         .io_in  (inputs),
         .io_out (outputs)
         );
